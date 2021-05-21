723538f2bde94c46e3054b39efbc71ffb45c395e
Erin Sabransky <82304053+N10203737@users.noreply.github.com>
2021-05-21 13:45:25 +1000

    Add the rest of the modules for leap motion. Created the foundation for gesture recognition and leap motion.

---
+++ UnityProject/Assets/ADS-Simulator/Scripts/LeapMotion/GestureManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureManager : MonoBehaviour
+{
+    public AnimationClip idle;
+    public AnimationClip die;
+    public GameObject deer1;
+    public GameObject deer2;
+
+    public void fist()
+    {
+        Debug.Log("Fist registered");
+
+        deer1.GetComponent<Animation>().clip = idle;
+        deer2.GetComponent<Animation>().clip = die;
+
+        deer1.GetComponent<Animation>().Play();
+        deer2.GetComponent<Animation>().Play();
+    }
+
+    public void ok()
+    {
+        Debug.Log("Okay registered");
+
+        deer1.GetComponent<Animation>().clip = die;
+        deer2.GetComponent<Animation>().clip = idle;
+
+        deer1.GetComponent<Animation>().Play();
+        deer2.GetComponent<Animation>().Play();
+    }
+
+    public void palm()
+    {
+        Debug.Log("Palm registered");
+
+        deer1.GetComponent<Animation>().Stop();
+        deer2.GetComponent<Animation>().Stop();
+    }
+}
