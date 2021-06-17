580938a4b3b668ecfe8c6013d65474fb88dd3e9f
Michael Batchelor <mickbatch98@gmail.com>
2021-06-18 00:55:20 +1000

    Client with simple UI that interacts with the Blinker

---
+++ UnityClient/Assets/AR-ADS-Client/Scripts/UI/UIWebController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWebController : MonoBehaviour
+{
+  public WebClient      WebClient;
+  public UnityEngine.UI.InputField BlinkDurationInput;
+  public UnityEngine.UI.InputField BlinkFrequencyInput;
+
+  public GestureService Gestures
+  {
+    get
+    {
+      if (_gestures == null && WebClient != null)
+        _gestures = WebClient.GetComponentInChildren<GestureService>();
+      return _gestures;
+    }
+  }
+  private GestureService _gestures;
+
+  public void ActivateBlinker()
+  {
+    Gestures.ActivateBlinker(float.Parse(BlinkDurationInput.text), float.Parse(BlinkFrequencyInput.text));
+  }
+
+  public void DeactivateBlinker()
+  {
+    Gestures.DeactivateBlinker();
+  }
+}
---
+++ UnityClient/Assets/AR-ADS-Client/Scripts/Web/Services/GestureService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureService : WebClientService
+{
+  public string StartCommand = "start";
+  public string StopCommand = "stop";
+
+  public string BlinkName = "Blink_Indicator";
+
+  string GetStartCommand(params object[] args)
+  {
+    return StartCommand + " " + string.Join(" ", args);
+  }
+
+  string GetStopCommand(params object[] args)
+  {
+    return StopCommand + " " + string.Join(" ", args);
+  }
+
+  public void ActivateBlinker(float duration = -1, float frequency = -1)
+  {
+    Send(GetStartCommand(BlinkName, duration, frequency));
+  }
+
+  public void DeactivateBlinker()
+  {
+    Send(GetStopCommand(BlinkName));
+  }
+
+  public override void HandleMessage(WebSocketSharp.MessageEventArgs messageData)
+  {
+  }
+}
