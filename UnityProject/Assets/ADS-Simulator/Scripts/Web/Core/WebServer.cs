043fe53ca6ae84fe2b5c35931dfcdfad9e976024
Mick Batchelor <mickbatch98@gmail.com>
2021-08-03 18:09:10 +1000

    small cleanup and comments

--- UnityProject/Assets/ADS-Simulator/Scripts/Web/Core/WebServer.cs
+++ UnityProject/Assets/ADS-Simulator/Scripts/Web/Core/WebServer.cs
@@ -7,45 +7,54 @@ using WebSocketSharp.Server;
 
 public class WebServer : MonoBehaviour
 {
+  // When service container.
+  // This is used to expose the server configuration to Unity's UI.
   [System.Serializable]
   public class WebServerService
   {
+    // This is the name of the service.
+    // You can connect to it using a URL 'some.host.name/Name'
     [SerializeField]
     public string Name;
 
+    // This object is duplicated when a service with the given name is created.
     [SerializeField]
     public Transform ServiceTemplate;
   }
 
-  public ushort Port = 8080; // The port to host the server on
-  public List<WebServerService> Services; // A list of services available on the server
-
-  public Transform ConnectedServicesParent = null;
-  private WebSocketServer Server = null; // WebSocketSharp server
-
   public delegate void Task();
 
+  // The port to host the server on
+  public ushort Port = 8080;
+  // A list of services available on the server
+  public List<WebServerService> Services;
+  // Where to pub newly connected/created services
+  public Transform ConnectedServicesParent = null;
+  // The WebSocketSharp object that handles the connectivity.
+  private WebSocketServer Server = null;
   // Tasks to be executed on the main thread
   public Queue<Task> TaskQueue = new Queue<Task>();
 
-  // Update the component
   public void Update()
   {
+    // We process any queued tasks in the Update so that the scene
+    // can be modified by the WebServerService's.
+    //
+    // This is needed as Unity only allows the scene to be modified
+    // on the main thread.
     while (TaskQueue.Count > 0)
       TaskQueue.Dequeue()();
   }
 
   void OnDestroy()
   {
-    // Stop the server from running
     Server.Stop();
   }
 
-  // Start is called before the first frame update
   void Start()
   {
     if (ConnectedServicesParent == null)
-    {
+    { // If the parent is not assigned, create somewhere to store the services
       ConnectedServicesParent = new GameObject().transform;
       ConnectedServicesParent.name = "Connected Services";
       ConnectedServicesParent.transform.parent = transform;
@@ -53,6 +62,7 @@ public class WebServer : MonoBehaviour
 
     // Create a WebSocketServer that is listening on the requested port
     Server = new WebSocketServer(Port);
+
     Debug.Log(string.Format("Hosting web server on {0}", Port));
 
     // Start the server
@@ -84,9 +94,7 @@ public class WebServer : MonoBehaviour
   }
 
   // A minimal wrapper of WebSocketBehaviour that forwards events received from WebSocketSharp
-  // to a unity game object specified. 
-  //
-  // It does this by adding tasks to a queue to be processed on the Main thread.
+  // to a unity game object specified.
   public class WebServiceWrapper : WebSocketBehavior
   {
     private Transform serviceParent   = null; // Where to place the new service in the scene hierarchy
@@ -95,23 +103,29 @@ public class WebServer : MonoBehaviour
     private WebService webService     = null; // The script that implements the service
     private WebServer  webServer      = null; // The parent server for this web service
     private bool createServiceFailed  = false;
+
     public WebServiceWrapper(Transform serviceTemplate, Transform serviceParent, WebServer server)
     {
       Debug.Log("Creating new service");
       webServer = server;
 
+      // Enqueue a task to duplicate the service template on the main thread.
       webServer.TaskQueue.Enqueue(() =>
       {
         try
         {
+          // Duplicate the template
           serviceObject = Instantiate(serviceTemplate);
+          // Place it under the serviceParent object used by the WebServer
           serviceObject.parent = serviceParent;
-
+          // Cache the WebService component so we don't need to find when sending/receiving messages.
           webService = serviceObject.GetComponent<WebService>();
           webService.SocketBehavior = this;
         }
         catch (System.Exception e)
         {
+          Debug.Log("Failed to create the service: " + e.Message);
+          // Notify the waiting thread if creating the service failed
           createServiceFailed = true;
         }
       });
@@ -121,22 +135,24 @@ public class WebServer : MonoBehaviour
     {
       // Destroy the service GameObject when this wrapper is destroyed
       Debug.Log("Destroying service");
-
     }
 
-    public void _Send(byte[] data) { base.Send(data); }
-    public void _Send(string data) { base.Send(data); }
-    public void _Send(FileInfo file) { base.Send(file); }
+    public void Send(byte[] data) { base.Send(data); }
+    public void Send(string data) { base.Send(data); }
+    public void Send(FileInfo file) { base.Send(file); }
 
-    public void _SendAsync(byte[] data, System.Action<bool> onComplete) { base.SendAsync(data, onComplete); }
-    public void _SendAsync(string data, System.Action<bool> onComplete) { base.SendAsync(data, onComplete); }
-    public void _SendAsync(FileInfo file, System.Action<bool> onComplete) { base.SendAsync(file, onComplete); }
-    public void _SendAsync(Stream stream, int length, System.Action<bool> onComplete) { base.SendAsync(stream, length, onComplete); }
+    public void SendAsync(byte[] data, System.Action<bool> onComplete) { base.SendAsync(data, onComplete); }
+    public void SendAsync(string data, System.Action<bool> onComplete) { base.SendAsync(data, onComplete); }
+    public void SendAsync(FileInfo file, System.Action<bool> onComplete) { base.SendAsync(file, onComplete); }
+    public void SendAsync(Stream stream, int length, System.Action<bool> onComplete) { base.SendAsync(stream, length, onComplete); }
 
     protected override void OnOpen()
     {
-      while (webService == null) {
-        Thread.Sleep(1); // Wait until our webservice has been created
+      // Wait until our webservice has been created
+      while (webService == null)
+      {
+        Thread.Sleep(1);
+
         if (createServiceFailed)
         {
           Debug.Log("Failed to create requested service");
@@ -144,27 +160,34 @@ public class WebServer : MonoBehaviour
           return;
         }
       }
+
+      // Queue the OnOpen event to be called.
       webServer.TaskQueue.Enqueue(() => webService.OnOpen());
     }
 
     protected override void OnClose(WebSocketSharp.CloseEventArgs e)
     {
       if (webService != null)
+      {
+        // Queue the OnClose event to be called.
         webServer.TaskQueue.Enqueue(() =>
         {
           webService.OnClose(e);
           Destroy(serviceObject.gameObject);
         });
+      }
     }
 
     protected override void OnError(WebSocketSharp.ErrorEventArgs e)
     {
+      // Queue the OnError event to be called.
       if (webService != null)
         webServer.TaskQueue.Enqueue(() => webService.OnError(e));
     }
 
     protected override void OnMessage(WebSocketSharp.MessageEventArgs e)
     {
+      // Queue the OnMessage event to be called.
       if (webService != null)
         webServer.TaskQueue.Enqueue(() => webService.OnMessage(e));
     }
