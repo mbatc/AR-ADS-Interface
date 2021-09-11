d452f4c305587909e6e07afd9f8a27856a372ab8
Mick Batchelor <mickbatch98@gmail.com>
2021-09-11 18:11:55 +1000

    Adding some gesture prefabs and  new scene for the traffic light interface

---
+++ Common/Interface/Scripts/Gestures/ScalarGesture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class ScalarGesture : GestureComponent
+{
+  [Tooltip("When the gesture is activated/deactivated, this target object will be activated/deactivated ")]
+  public Transform Target;
+  
+  public float CurrentValue;
+
+  public override void ActivateGesture(JSONObject args)
+  {
+    if (Target != null)
+      Target.gameObject.SetActive(true);
+  }
+
+  public override void DeactivateGesture(JSONObject args)
+  {
+    if (Target != null)
+      Target.gameObject.SetActive(false);
+  }
+
+  public virtual void UpdateGesture(JSONObject args)
+  {
+    CurrentValue = args["value"].AsFloat();
+  }
+}
